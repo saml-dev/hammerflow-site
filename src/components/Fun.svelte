b2a1a5e73613cbca3cc2bc5fd4582fe455049725
Sam Lewis <sam.lewis0602@gmail.com>
2025-02-25 08:57:00 -0500

    "fun" animation is there...we'll see if I keep it as is

--- src/components/Fun.svelte
+++ src/components/Fun.svelte
@@ -1,17 +1,19 @@
 <script>
-  import { quartOut } from 'svelte/easing';
+  import { onMount } from 'svelte';
+  import { quartIn } from 'svelte/easing';
+
   export function tapeSlap(node, { duration = 200, delay = 1500 }) {
     const transform = getComputedStyle(node).transform.replace('none', '');
     return {
       delay,
       duration,
       css: (t) => {
-        const eased = quartOut(t);
+        const eased = quartIn(t);
         const scale = 1.7 - 0.7 * eased; // Start larger, scale down
         const x = -25 * eased + 25; // Move left
         const y = 50 * eased - 50; // Move down
-        const z = 200 * eased - 200; // move "into" screen
-        const opacity = eased > 0.03 ? 1 : 0;
+        const z = 400 * eased - 400; // move "into" screen
+        const opacity = eased > 0.2 ? 1 : 5 * eased;
         return `
           opacity:${opacity};
           transform: ${transform}
@@ -26,15 +28,16 @@
 
 <span
   in:tapeSlap
-  class="fun-overlay hover:opacity-0 rounded tracking-widest transition-all whitespace-nowrap px-[1em] py-[.1em]"
+  aria-label="and fun"
+  class="fun-overlay group-hover:opacity-0! rounded tracking-widest transition-all whitespace-nowrap px-[.7em] py-[.1em]"
 >
-  fun
+  fun 🎉
 </span>
 
 <style lang="postcss">
   .fun-overlay {
     position: absolute;
-    top: 50%;
+    top: 35%;
     left: 50%;
     transform: translate(-50%, -50%) rotate(-7deg);
     background-color: #c1f8d3;
---
+++ src/components/Tagline.svelte
@@ -0,0 +1,3 @@
+<script>
+  import Fun from './Fun.svelte';
+</script>
