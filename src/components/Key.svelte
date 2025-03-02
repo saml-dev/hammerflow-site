9d245da78d3adbb8c0a342522b2113536aae9af8
Sam Lewis <sam.lewis0602@gmail.com>
2025-03-02 17:13:56 -0500

    commit to see if better on safari in preview deployment

--- src/components/Key.svelte
+++ src/components/Key.svelte
@@ -5,15 +5,16 @@
 </script>
 
 <kbd
-  class="px-5 py-3 text-lg font-medium font-mono transition-all text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer select-none"
-  in:fly={{ y: -4, duration: 200, delay: idx * 110 }}
-  out:fade
+  class="animated-key px-5 py-3 text-lg font-medium font-mono text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer select-none"
 >
   {@render children()}
 </kbd>
 
 <style>
   kbd {
+    transition:
+      margin 100ms ease-in-out,
+      box-shadow 100ms ease-in-out;
     margin-top: 2px;
     margin-bottom: 3px;
     box-shadow: 0px 3px 0px 0px #00000024;
