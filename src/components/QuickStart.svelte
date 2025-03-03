d4cb903f4b9551b0ff777bd7a9d533ddfa2c6bab
Sam Lewis <sam.lewis0602@gmail.com>
2025-03-02 20:15:58 -0500

    remove blend mode

--- src/components/QuickStart.svelte
+++ src/components/QuickStart.svelte
@@ -6,14 +6,14 @@
 <div>
   <h2>Quick Start</h2>
   <div
-    class="mt-3 bg-white/10 backdrop-blur-3xl rounded-lg shadow-sm border border-gray-800/10"
+    class="mt-3 bg-white/20 backdrop-blur-3xl rounded-lg shadow-sm border border-gray-800/10"
   >
     <div class="w-full grid grid-cols-2">
       <button
         class={{
           'py-3 px-1 rounded-tl-lg text-sm md:text-base text-balance': true,
           'border-b border-b-gray-200': !firstTab,
-          'bg-soft-yellow bg-blend-multiply': firstTab,
+          'bg-soft-yellow': firstTab,
         }}
         onclick={() => (firstTab = true)}
       >
@@ -23,12 +23,12 @@
         class={{
           'py-3 px-1 rounded-tr-lg text-sm md:text-base text-balance': true,
           'border-b border-b-gray-200': firstTab,
-          'bg-soft-yellow bg-blend-multiply': !firstTab,
+          'bg-soft-yellow': !firstTab,
         }}
         onclick={() => (firstTab = false)}>I already use hammerspoon</button
       >
     </div>
-    <div class="p-3 pt-8 bg-soft-yellow bg-blend-multiply rounded-b-lg">
+    <div class="p-3 pt-8 bg-soft-yellow rounded-b-lg">
       {#if firstTab}
         {@render first()}
       {:else}
